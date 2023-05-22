63ecd6f2f88c35bf9f929e1cc742c95597058217
Jenn <121206502+Jennefer19@users.noreply.github.com>
2023-05-22 13:24:39 +0800

    Add files via upload

---
+++ lab5/about.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>My Website</title>
+  <link type="text/css" rel="stylesheet" href="mystyle.css" media="screen">
+</head>
+<body>
+  <nav>
+    <ul>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="about.php">About</a></li>
+    </ul>
+  </nav>
+  <header>
+    <h1>Labotatory 5</h1>
+  </header>
+  <main id="main">
+    <h2> coffee</h2>
+    <img class="center" src="bg1.jpg" alt="">
+    <p>If I only had one day left to live I'd choose to sit on my swing out back all the damn day long with my cat on my lap and my hands wrapped around a near <br> empty mug of coffee, knowing there was more in the pot in the kitchen. Preferably after reading a much emoji filled text from my sweet granddaughter.</p>
+  </main>
+  <footer>
+    <h4> by: <br>Jennefer Catalan</h4>
+  </footer>
+</body>
+</html>
---
+++ lab5/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>My Website</title>
+  <link type="text/css" rel="stylesheet" href="mystyle.css" media="screen">
+</head>
+<body>
+  <nav>
+    <ul>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="about.php">About</a></li>
+    </ul>
+  </nav>
+  <header>
+    <h1>Labotatory 5</h1>
+  </header>
+  <main id="main">
+    <h2> coffee</h2>
+    <img class="center" src="bg.jpeg" alt="">
+    <p>True love these days can be found in a lot of creative places—online, at a bar, or the old-fashioned way, on a blind date. But to a true coffee lover, the <br> relationship they want and need is found every morning (noon and night, sometimes!) at the bottom of their favorite mug. Black, cream, sugar, foam, no <br> foam, these preferences could easily divide nations, it seems, because people LOVE their coffee a certain and often will not budge on changing it. <br> Growing up in Seattle, where Starbucks is the Mothership, and craft coffee shops are found on every corner, I grew up thinking coffee and coffee culture <br> was a normal way of life. But for many people, coffee is just what gets you going in the morning. You fill up your trusty thermos or travel mug with <br> whatever you’ve got ground up or in a pod and you’re off for the day. So if that’s you, and you’d like to experience a deeper affinity and appreciation <br> for one of God’s finest beverages, then look no further than these quotes. And if you’re on the hunt for what to buy your coffee-crazed friend or <br> family member, look no further than our coffee lover gift guide!</p>
+  </main>
+  <footer>
+    <h4> by: <br>Jennefer Catalan</h4>
+  </footer>
+</body>
+</html>
